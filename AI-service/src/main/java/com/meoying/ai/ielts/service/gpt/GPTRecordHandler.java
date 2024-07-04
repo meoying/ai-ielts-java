48a307a73e7706a109f14e11a787603ceaba854e
Deng Ming <ming_flycash@qq.com>
2024-07-04 17:15:05 +0800

    定义扣费接口

---
+++ AI-service/src/main/java/com/meoying/ai/ielts/service/gpt/GPTRecordHandler.java
@@ -0,0 +1,10 @@
+package com.meoying.ai.ielts.service.gpt;
+
+// GPTRecordHandler
+// 将 GPT 的调用结果记录到数据库里面
+public class GPTRecordHandler implements Handler{
+    @Override
+    public Response handle(Request req) {
+        return null;
+    }
+}
